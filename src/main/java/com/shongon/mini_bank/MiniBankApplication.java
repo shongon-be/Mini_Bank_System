592767f829f017b048d15a2d9ace0654da89fe37
Hong Son <hongsonn539@gmail.com>
2025-04-17 14:33:19 +0700

    init: initial commit

---
+++ src/main/java/com/shongon/mini_bank/MiniBankApplication.java
@@ -0,0 +1,13 @@
+package com.shongon.mini_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBankApplication.class, args);
+	}
+
+}
